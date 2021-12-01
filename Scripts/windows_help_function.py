c60515b9d41bb9a7799735540f17bb94c05ce6d7
Griiffiin <49454046+Griiffiin@users.noreply.github.com>
2021-11-30 19:21:07 -0500

    Add files via upload

---
+++ Scripts/windows_help_function.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import re
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+nltk.download('vader_lexicon')
+
+from pandas.core.indexing import convert_from_missing_indexer_tuple
+
+def get_conv_num(a_csv):
+    df = pd.read_csv(a_csv)
+    have_url_list = []
+    for i in range(df.shape[0]):
+        if (df.iloc[i]["message"].count('http', 0, len(df.iloc[i]["message"]))):
+            if df.iloc[i]["thread"] not in have_url_list:
+                have_url_list.append(df.iloc[i]["thread"])
+    return  have_url_list
+
+def get_conv(a_conv_number,a_csv):
+    conv_context = []
+    df = pd.read_csv(a_csv)
+    for i in range(df.shape[0]):
+        if(df.iloc[i]["thread"]) == a_conv_number:
+            conv_context.append({"speaker": df.iloc[i]["speaker"], "message": df.iloc[i]["message"]})
+    f = pd.DataFrame(conv_context, columns=["speaker", "message"])
+    f.to_csv("./test_conv.csv", index=False)
+    ##print(conv_context)
+
+def get_new_frame(id, a_csv):
+    conv_context = []
+    df = pd.read_csv(a_csv)
+    for i in range(df.shape[0]):
+        if "http" in df.iloc[i]["message"]:
+            if (i != df.shape[0]-1):
+                conv_context.append({"id": id, "Response": df.iloc[i+1]["message"]})
+            else:
+                conv_context.append({"id": id, "Response": "Nothing there"})
+    f = pd.DataFrame(conv_context, columns=["id", "Questioner","Question","Response"])
+    f.to_csv("./test_conv_finding.csv", index=False)
+
+def get_GOLD_SET(ids, a_csv):
+    conv_context = []
+    df = pd.read_csv(a_csv)
+    count = 0
+    for each_id in ids:
+        for i in range(df.shape[0]):
+            if df.iloc[i]["thread"] == each_id:
+                if "http" in df.iloc[i]["message"]:
+                    if (i != df.shape[0]-1):
+                        conv_context.append({"id": each_id,"Response": df.iloc[i+1]["message"]})
+                    else:
+                        conv_context.append({"id": each_id,"Response": "Nothing there"})
+        count += 1
+        if count == 100:
+            break
+    f = pd.DataFrame(conv_context, columns=["id","Response"])
+    f.to_csv("./for_gold_set.csv", index=False)
+
+#def get_http(a_csv):
+#    url_set = []
+#    df = pd.read_csv(a_csv)
+#    for i in range(df.shape[0]):
+#        if 'http' in df.iloc[i]['message']:
+#            url_set.append(re.search("(?P<url>https?://[^\s]+)", df.iloc[i]['message']).group("url").split(">")[0])
+
+#    return url_set
+
+def sentimental(a_csv):
+    data = []
+    df = pd.read_csv(a_csv)
+    for i in range(df.shape[0]):
+        if(df.iloc[i]["Response"] == "Nothing there"):
+            return("Cannot judge the URL")
+        else:
+            data.append(df.iloc[i]["Response"])
+
+    sid = SentimentIntensityAnalyzer()
+    pos_score = 0
+    neu_score = 0
+    neg_score = 0
+
+    for sen in data:
+        ss = sid.polarity_scores(sen)
+        for k in ss:
+            if (k == 'neg'):
+                neg_score += ss[k]
+            elif(k == 'neu'):
+                neu_score += ss[k]
+            elif(k == 'pos'):
+                pos_score += ss[k]
+    if(pos_score+neg_score > 0.03):
+        if(pos_score > neg_score):
+            return("Positive")
+        else:
+            return("Negative")
+    else:
+        return("Undetermined")
+
+
+
+
---
+++ Scripts/windows_py.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+#def analysis(x):
+#from help_function import analysis, count, compare, count_chat_author
+from windows_help_function import get_conv_num, get_conv, get_new_frame, sentimental, get_GOLD_SET
+
+df = pd.read_csv('merged-pythondev-help-concat-group.csv')
+
+hul = get_conv_num('merged-pythondev-help-concat-group.csv')
+print(hul)
+
+answer_list = []
+
+pos_data = 0
+neu_data = 0
+neg_data = 0
+cant_data = 0
+
+get_GOLD_SET(hul,'merged-pythondev-help-concat-group.csv')
+for i in hul:
+    get_conv(i,'merged-pythondev-help-concat-group.csv')
+    get_new_frame(i, 'test_conv.csv')
+    #print(get_http('test_conv.csv'))
+    #print(i)
+    #print(sentimental('test_conv_finding.csv'))
+    answer_list.append({"Conv ID": i, "Result" : sentimental('test_conv_finding.csv')})
+
+f = pd.DataFrame(answer_list, columns=["Conv ID", "Result"])
+f.to_csv("./final_result.csv", index=False)
+
+df = pd.read_csv("./final_result.csv")
+for i in range(df.shape[0]):
+    if (df.iloc[i]["Result"] == "Positive"):
+        pos_data += 1
+    elif (df.iloc[i]["Result"] == "Negative"):
+        neg_data += 1
+    elif (df.iloc[i]["Result"] == "Undetermined"):
+        neu_data += 1
+    else:
+        cant_data += 1
+
+
+total_data = pos_data + neg_data + neu_data + cant_data
+print("The percentage of positive data is: ",pos_data/total_data)
+print("The percentage of negative data is: ",neg_data/total_data)
+print("The percentage of undetermined data is: ",neu_data/total_data)
+print("The percentage of cannot judge data is: ",cant_data/total_data)
+#get_conv(3,'merged-pythondev-help-concat-group.csv')
+
+#get_new_frame(3,'test_conv.csv')
+#print(get_http('test_conv.csv'))
+#print(sentimental('test_conv_finding.csv'))
